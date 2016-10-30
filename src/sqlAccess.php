7e61499af124bc884b250ff0263e9d33d33c4e67
RodgerTheShrubber <tbrumley96@gmail.com>
2016-10-29 20:54:11 -0400

    Changed sqlAccess

--- src/sqlAccess.php
+++ src/sqlAccess.php
@@ -5,39 +5,20 @@ $password = "Hacknc2016!";
 $dbname = "CheckiNC";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// prepare and bind
-$stmt = $conn->prepare("INSERT INTO Person ( fName, lName, email) VALUES (?, ?, ?)");
-$stmt->bind_param("sss", $fName, $lName, $email);
-
-// set parameters and execute
-$fName = $_POST['fName'];
-$email= $_POST['email'];
-$eDescription = $_POST['lName']
-$stmt->execute();
-
-echo "New person record has been created successfully";
-
-$stmt->close();
-
-// prepare and bind
-$stmt = $conn->prepare("INSERT INTO EventInfo ( eventName, hostEmail, description) VALUES (?, ?, ?)");
-$stmt->bind_param("sss", $eName, $email, $eDescription);
-
-// set parameters and execute
-$eName = $_POST['eName'];
-$email= $_POST['email'];
-$eDescription = $_POST['eDescription']
-$stmt->execute();
-
-echo "New event record has been created successfully";
-
-$stmt->close();
-$conn->close();
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$CheckiNC", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$fName = $_POST['fName'];
+	$email= $_POST['email'];
+	$lname = $_POST['lName'];
+	$sql = "INSERT INTO Person ( fName, lName, email) VALUES ('$fName', '$lName', $'email')";
+    $conn->exec($sql);
+	echo "New record created";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+$conn = null;
 ?> 
