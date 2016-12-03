cae6a5adbacd591eaea859f5da878d5a8eba2031
KyleMartin95 <kylemartin95@gmail.com>
2016-12-03 18:03:47 -0500

    added ajax methods to main.js;

--- src/js/MapFunctions.js
+++ src/js/MapFunctions.js
@@ -1,21 +1,23 @@
 var markers = [];
+var circles =[];
 
 function myMap() {
     debugger;
 
-
-
     //sets the div to draw the map on
     var mapCanvas = document.getElementById("map");
-    
+
     //specifies custom options for map
     var mapOptions = {
         center: new google.maps.LatLng(51.508742,-0.120850),
         zoom: 5
     };
-    
+
     //creates map
     var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var infowindow = new google.maps.InfoWindow({
+    });
 
 
     //gets user location and sets map to that location
@@ -48,7 +50,8 @@ function myMap() {
 
     //adds click listener and calls functions that set a marker and specifies lat long at click location
    	map.addListener('click', function(e) {
-  		placeMarker(map, e.latLng);
+  		//placeMarker(map, e.latLng);
+  		placeCircle(map, e.latLng);
   		setLatLong(e.latLng);
   	});
 }
@@ -64,14 +67,45 @@ function placeMarker(map, location) {
 }
 
 function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
+    for (var i = 0; i < circles.length; i++){
+        if(i < circles.length){
+            circles[i].setMap(map);
+        }
     }
 }
 
 // Removes the markers from the map, but keeps them in the array.
 function clearMarkers() {
 	setMapOnAll(null);
+}
+
+function clearCircles(){
+    setMapOnAll(null);
+}
+
+function placeCircle(map, location){
+    debugger;
+    clearCircles();
+	var circle = new google.maps.Circle({
+		map: map,
+		center: location,
+		radius: 50,
+		strokeColor: "#0000FF",
+		strokeOpacity: 0.8,
+		strokeWeight: 2,
+		fillColor: "#0000FF",
+		fillOpacity: 0.2,
+		editable: true
+	});
+    circles.push(circle);
+}
+
+function getCircleRadius(circle){
+    return radius = circle.radius;
+}
+
+function getCircleCenter(circle){
+    return center = circle.center;
 }
 
 //function called on click event to set long and lat on the form for submission
@@ -85,4 +119,3 @@ function setLatLongGeo(position){
 	document.getElementById("longitude").value = position.coords.longitude;
 	document.getElementById("latitude").value = position.coords.latitude;
 }
-
