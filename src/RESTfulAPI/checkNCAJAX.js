83ec5cd4fd5272c89297ef01e85b1193d0604923
robobigfoot <ericnew@live.unc.edu>
2016-12-06 13:57:37 -0500

    Add files via upload

---
+++ src/RESTfulAPI/checkNCAJAX.js
@@ -0,0 +1,36 @@
+var url_base = "https://wwwp.cs.unc.edu/Courses/comp426-f16/users/kykyle/Final_Project/src/RESTfulAPI/checkNC.php";
+
+
+var post_event = function(firstname, lastname, email, eventname, latitude, longitude, radius, numberAttending, starttime, endtime, description){
+	$.ajax(url_base + "/MasterEventCreate",
+	{type: "POST",
+		datatype: "json",
+		data:{
+			"FName": firstname,
+			"LName": lastname,
+			"Email": email,
+			"EventName": eventname,
+			"Latitude": latitude,
+			"Longitude": longitude,
+			"Radius": radius,
+			"NumberAttending": numberAttending,
+			"StartTime": starttime,
+			"EndTime": endtime,
+			"Description": description
+		},
+		});
+	};
+
+var attend_event = function(firstname, lastname, email, eventname) {
+	$.ajax(url_base + "/MasterEventCheckIn",
+	{type: "POST",
+		datatype: "json",
+		data:{
+			"FName": firstname,
+			"LName": lastname,
+			"Email": email,
+			"EventName": eventname
+		},
+		});
+	};
+
